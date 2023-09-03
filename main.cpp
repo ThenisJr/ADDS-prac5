5e242f6d230fefc14b6ba9dbcb774ffb327d8b43
Dimos Karagiannis <dtkaragiannis@gmail.com>
2023-09-03 11:14:56 +0930

    and main

--- main.cpp
+++ main.cpp
@@ -9,24 +9,6 @@ int main() {
     QuickSort quip;
     RecursiveBinarySearch search;
     std::vector<int> vec = {5, 3, 20, 15, 4, 2, 200};
-    std::vector<int> vec5 = {1, 3, 4, 5, 10, 20, 30};
-    //std::vector<int> vec2 = {1, 0, 2, 1, 4, 5, 3, 3};
-    //std::vector<int> vec3 = {1, 0, 3, 1, 4, 5, 3, 2};
     std::cout<<search.search(vec5, 31)<<std::endl;
-    /*std::vector<int> sorted = bub.sort(vec);
-    std::cout<<"bubble: "<<std::endl;
-    for(int i = 0;i<sorted.size();i++){
-        std::cout<<sorted.at(i)<<std::endl;
-    }
-    std::vector<int> sorted2 = qui.sort(vec2);
-    std::cout<<"quickL: "<<std::endl;
-    for(int i = 0;i<sorted2.size();i++){
-        std::cout<<sorted2.at(i)<<std::endl;
-    }
-    std::vector<int> sorted3 = quip.sort(vec3);
-    std::cout<<"quick: "<<std::endl;
-    for(int i = 0;i<sorted3.size();i++){
-        std::cout<<sorted3.at(i)<<std::endl;
-    }*/
     return 1;
 }
